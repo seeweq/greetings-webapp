2bd9d0a9ba09aee37347934194a282c012d8797a
sivuyisiwe <sivuyisiwequse0@gmail.com>
2017-08-23 16:41:51 +0200

    edit files

--- model.js
+++ model.js
@@ -1,12 +1,14 @@
 var mongoose = require('mongoose')
 const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/greetedNames";
+
+console.log(mongoURL);
+
 mongoose.connect(mongoURL, {
   useMongoClient: true
 });
 
 mongoose.connection.on("error", function(err){
   console.log(err);
-  console.log(arguments);
 });
 
 exports.PeopleGreeted = mongoose.model('peoplegreeteds', {
